f7375591d9ab916712aa1ba206a47fb988de13e8
Jonas Köhnen <mail@satoqz.net>
2025-01-26 03:21:57 +0100

    refactor(proxy): simplify error handling

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,65 +1,60 @@
 use std::fmt;
 
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::{Extension, Router};
-use sentry::protocol::Map;
-use sentry::Breadcrumb;
-use serde_json::Value;
 
 use crate::calendar::Calendar;
-use crate::helpers;
 use crate::resolver::UpstreamUrlExtension;
 
 #[derive(Debug)]
-struct Error {
-    message: &'static str,
-    kind: ErrorKind,
-}
-
-#[derive(Debug)]
-enum ErrorKind {
-    Reqwest(reqwest::Error),
-    Status(reqwest::StatusCode),
+enum Error {
+    Request(reqwest::Error),
     Parse(crate::parser::Error),
 }
 
-impl Error {
-    pub fn new(message: &'static str, kind: ErrorKind) -> Self {
-        Self { message, kind }
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let message = match &self {
+            Self::Request(err) if err.is_status() => "Upstream returned unexpected status code",
+            Self::Request(_) => "Can't connect to upstream",
+            Self::Parse(_) => "Can't parse calendar",
+        };
+        write!(f, "{message}")
     }
 }
 
 impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match &self.kind {
-            ErrorKind::Reqwest(err) => Some(err),
-            ErrorKind::Parse(err) => Some(err),
-            ErrorKind::Status(_) => None,
-        }
+        Some(match self {
+            Self::Request(err) => err,
+            Self::Parse(err) => err,
+        })
     }
 }
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
+impl From<reqwest::Error> for Error {
+    fn from(value: reqwest::Error) -> Self {
+        Self::Request(value)
     }
 }
 
-impl Error {
-    fn capture(self) -> Self {
-        sentry::capture_error(&self);
-        self
+impl From<crate::parser::Error> for Error {
+    fn from(value: crate::parser::Error) -> Self {
+        Self::Parse(value)
     }
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
-        let status = match self.kind {
-            ErrorKind::Reqwest(_) => StatusCode::BAD_GATEWAY,
-            ErrorKind::Parse(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            ErrorKind::Status(status) => status, // Propagate whatever issue they're having.
+        let status = match &self {
+            Self::Request(err) if err.is_status() => {
+                err.status().expect("error status should be set")
+            } // Propagate whatever issue they're having.
+            Self::Request(_) => StatusCode::BAD_GATEWAY,
+            Self::Parse(_) => StatusCode::INTERNAL_SERVER_ERROR,
         };
 
         (
@@ -82,76 +77,21 @@ impl IntoResponse for Calendar {
 }
 
 pub fn apply_routes(router: Router) -> Router {
-    router.route("/{*path}", get(handle_calendar))
+    const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+    let client = reqwest::Client::builder()
+        .user_agent(USER_AGENT)
+        .build()
+        .expect("reqwest client should build");
+
+    router.route("/{*path}", get(handle_calendar).with_state(client))
 }
 
 async fn handle_calendar(
+    State(client): State<reqwest::Client>,
     Extension(upstream): Extension<UpstreamUrlExtension>,
-) -> impl IntoResponse {
-    breadcrumb("Sending request to Rapla", "http", {
-        helpers::map!({ "method": "GET", "url": upstream.url })
-    });
-
-    let response = send_request(&upstream.url).await?;
-    let status = response.status();
-
-    breadcrumb("Got response from Rapla", "http", {
-        helpers::map!({ "method": "GET", "url": upstream.url, "status_code": status.as_u16() })
-    });
-
-    if !status.is_success() {
-        return Err(Error::new(
-            "Upstream returned bad status code",
-            ErrorKind::Status(status),
-        ));
-    }
-
-    let html = response.text().await.map_err(|err| {
-        Error::new(
-            "Couldn't parse body returned by upstream",
-            ErrorKind::Reqwest(err),
-        )
-        // I'd be curious to know if this ever occurs.
-        .capture()
-    })?;
-
-    crate::parser::parse_calendar(&html, upstream.start_year).map_err(|err| {
-        Error::new(
-            "Couldn't parse HTML returned by upstream",
-            ErrorKind::Parse(err),
-        )
-        // These are the important errors we really want to track.
-        // Given that Rapla returned a successful status code for a set of well-formed
-        // query parameters, we can be at least 90% certain that our parsing is broken
-        // (or was broken, depending on how you see it).
-        .capture()
-    })
-}
-
-fn breadcrumb(message: &str, ty: &str, data: Map<String, Value>) {
-    sentry::add_breadcrumb(Breadcrumb {
-        ty: ty.into(),
-        category: Some("proxy".into()),
-        message: Some(message.into()),
-        data,
-        ..Default::default()
-    });
-}
-
-async fn send_request(url: &str) -> Result<reqwest::Response, Error> {
-    let user_agent = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
-    let client = reqwest::Client::builder()
-        .user_agent(user_agent)
-        .build()
-        .map_err(|err| Error::new("Couldn't connect to upstream", ErrorKind::Reqwest(err)))?;
-
-    let request = client
-        .get(url)
-        .build()
-        .map_err(|err| Error::new("Couldn't connect to upstream", ErrorKind::Reqwest(err)))?;
-
-    client
-        .execute(request)
-        .await
-        .map_err(|err| Error::new("Request to upstream failed", ErrorKind::Reqwest(err)))
+) -> Result<Response, Error> {
+    let request = client.get(&upstream.url).build()?;
+    let response = client.execute(request).await?.error_for_status()?;
+    let html = response.text().await?;
+    Ok(crate::parser::parse_calendar(&html, upstream.start_year)?.into_response())
 }
